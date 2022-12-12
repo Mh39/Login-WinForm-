fbb0ebd70e487d6dc2da3666c3ee80fd078234eb
Mohamed Hassan <mh0056592@gmail.com>
2022-12-12 17:43:03 +0200

    commit

---
+++ Login Screen WinForm/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Login_Screen_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Enter The username");
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("Enter The password");
+
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection conn = new SqlConnection(@"Server = LAPTOP-JFIAGS6G\SQLEXPRESS ; Database = LoginForm ; Integrated Security = SSPI ; TrustServerCertificate = True");
+
+                    SqlCommand cmd = new SqlCommand("select * from tbl_login where upper (Username)= upper (@usename) and Password=@Password;", conn);
+                    conn.Open();
+                    cmd.Parameters.AddWithValue("@usename", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@Password", textBox2.Text);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+
+
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        string Active_User = dt.Rows[0]["Active"].ToString().TrimStart();
+
+                        if (Active_User == "Yes                                               ")
+                        {
+
+                            MessageBox.Show("Login Successful");
+                        }
+                        else
+                        {
+                            MessageBox.Show("invalid");
+
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("useName or password is invalid");
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Login Screen WinForm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Login_Screen_WinForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
